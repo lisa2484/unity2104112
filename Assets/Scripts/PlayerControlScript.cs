44d11f3b887ee1951da5dbc2b87382935e316ee2
lisa2484 <ablt2484@gmail.com>
2021-04-11 16:53:37 +0800

    修改

---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) this.GetComponent<Animator>().SetBool("Run", true);
+        else this.GetComponent<Animator>().SetBool("Run", false);
+    }
+}
