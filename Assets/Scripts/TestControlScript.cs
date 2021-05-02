7f999497b8a4d48d886e1ea276e1c7d57163ec00
lisa2484 <ablt2484@gmail.com>
2021-05-02 13:04:12 +0800

    增加移動方式及圖片

--- Assets/Scripts/TestControlScript.cs
+++ Assets/Scripts/TestControlScript.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
-using System;
 
 public class TestControlScript : MonoBehaviour
 {
-    public Camera camera;
+    public Camera MyCamera;
     void Start()
     {
     }
 
     void Update()
     {
-        // Vector2 mouse = new Vector2(camera.ScreenToViewportPoint.x, Input.mousePosition.y);
-        // Quaternion agin = new Quaternion(0, 0, Vector2.Angle(new Vector2(transform.position.x, transform.position.y), mouse), 0);
-        // Debug.Log(mouse);
-        // Debug.Log(new Vector2(transform.position.x, transform.position.y));
-        // Debug.Log(Vector2.Angle(new Vector2(transform.position.x, transform.position.y), mouse));
-        // transform.GetChild(0).rotation = agin;
+        Vector2 mouse = MyCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 MousePosition = (new Vector2(transform.position.x, transform.position.y) - mouse) * -1;
+        if (Input.GetMouseButton(1))
+        {
+            GetComponent<Rigidbody2D>().AddForce(MousePosition);
+        }
     }
 }
